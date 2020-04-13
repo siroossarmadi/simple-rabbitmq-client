2625750e4fd8c1d04e1049c765aa2f1c210789b0
siroossarmadi <siroos.sarmadi@gmail.com>
2020-04-13 20:21:22 +0430

    Add get and delete method to client

--- rabbitmq_client/__init__.py
+++ rabbitmq_client/__init__.py
@@ -1,4 +1,5 @@
 import pika
+import pickle
 
 
 class RabbitmqClient:
@@ -11,9 +12,14 @@ class RabbitmqClient:
     def pull(self, callback, queue, durable=True):
         self.channel.queue_declare(queue=queue, durable=durable)
         self.channel.basic_qos(prefetch_count=1)
-        self.channel.basic_consume(queue=queue,
-                                   on_message_callback=callback)
+        self.channel.basic_consume(queue, on_message_callback=callback)
         self.channel.start_consuming()
+
+    def get(self, queue, durable=True):
+        self.channel.queue_declare(queue=queue, durable=durable)
+        self.channel.basic_qos(prefetch_count=1)
+        method, _, body = self.channel.basic_get(queue=queue)
+        return body, method.delivery_tag
 
     def push(self, message, routing_key, exchange='', delivery_mode=2):
         self.channel.basic_publish(
@@ -23,3 +29,9 @@ class RabbitmqClient:
             properties=pika.BasicProperties(
                 delivery_mode=delivery_mode,
             ))
+
+    def delete(self, queue, delivery_tag, ack):
+        if ack:
+            self.channel.basic_ack(delivery_tag=delivery_tag)
+        else:
+            self.channel.basic_nack(delivery_tag=delivery_tag)
