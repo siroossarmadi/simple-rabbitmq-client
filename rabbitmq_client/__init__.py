cbe1b699217e7a3a584fc3f60a65992ed6a79255
siroossarmadi <siroos.sarmadi@gmail.com>
2020-05-03 22:29:38 +0430

    Change self params

--- rabbitmq_client/__init__.py
+++ rabbitmq_client/__init__.py
@@ -3,29 +3,29 @@ import pika
 
 class RabbitmqClient:
     def __init__(self, url):
-        self.__connection = pika.BlockingConnection(pika.URLParameters(url))
-        self.__channel = self.__connection.channel()
+        self._connection = pika.BlockingConnection(pika.URLParameters(url))
+        self._channel = self._connection.channel()
 
     def get_channel(self):
-        return self.__channel
+        return self._channel
 
     def pull(self, callback, queue, durable=True):
         """
         Callback params : channel, method, properties, body
         """
-        self.__channel.queue_declare(queue=queue, durable=durable)
-        self.__channel.basic_qos(prefetch_count=1)
-        self.__channel.basic_consume(queue, on_message_callback=callback)
-        self.__channel.start_consuming()
+        self._channel.queue_declare(queue=queue, durable=durable)
+        self._channel.basic_qos(prefetch_count=1)
+        self._channel.basic_consume(queue, on_message_callback=callback)
+        self._channel.start_consuming()
 
     def get(self, queue, durable=True):
-        self.__channel.queue_declare(queue=queue, durable=durable)
-        self.__channel.basic_qos(prefetch_count=1)
-        method, _, body = self.__channel.basic_get(queue=queue)
+        self._channel.queue_declare(queue=queue, durable=durable)
+        self._channel.basic_qos(prefetch_count=1)
+        method, _, body = self._channel.basic_get(queue=queue)
         return body, method
 
     def push(self, message, routing_key, exchange='', delivery_mode=2):
-        self.__channel.basic_publish(
+        self._channel.basic_publish(
             exchange=exchange,
             routing_key=routing_key,
             body=message,
@@ -35,6 +35,6 @@ class RabbitmqClient:
 
     def delete(self, queue, delivery_tag, ack):
         if ack:
-            self.__channel.basic_ack(delivery_tag=delivery_tag)
+            self._channel.basic_ack(delivery_tag=delivery_tag)
         else:
-            self.__channel.basic_nack(delivery_tag=delivery_tag)
+            self._channel.basic_nack(delivery_tag=delivery_tag)
